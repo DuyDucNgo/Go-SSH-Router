b74203081fa5085da33b72b4ed265ac6a526fc45
DuyDucNgo <93795735+DuyDucNgo@users.noreply.github.com>
2022-09-09 17:46:38 +0100

    Added main

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	//var hostKey ssh.PublicKey
+	config := &ssh.ClientConfig{
+		User: "ccna",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("vnpro"),
+		},
+		//HostKeyCallback: ssh.FixedHostKey(hostKey),
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", "172.16.1.1:22", config)
+	if err != nil {
+		log.Fatal("Failed: ", err)
+	}
+	defer client.Close()
+	// session
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Faile to create session", err)
+	}
+	defer session.Close()
+	// run single command
+	var b bytes.Buffer
+	session.Stdout = &b
+	cli := "show ip interface brief"
+	if err := session.Run(cli); err != nil {
+		log.Fatal("failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+}
